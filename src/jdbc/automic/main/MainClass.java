d2dbb0713dbede49501cf39452ebd0d8b86ffd89
Szemelliker <szj@automic.com>
2017-07-17 11:37:35 +0200

    Add DBConnector

--- src/jdbc/automic/main/MainClass.java
+++ src/jdbc/automic/main/MainClass.java
@@ -1,11 +1,13 @@
 package jdbc.automic.main;
 
+import java.sql.SQLException;
+
 import jdbc.automic.configuration.Configuration;
 import jdbc.automic.dbconnector.DBConnector;
 import jdbc.automic.restconnector.RestConnector;
 
 public class MainClass {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		new Configuration();
 		new DBConnector();
 		new RestConnector();
