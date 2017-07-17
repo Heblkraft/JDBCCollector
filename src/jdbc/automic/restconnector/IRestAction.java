4a0b13faf734c1da13cccfdcf6ef211b9d099648
Niederer <nic@automic.com>
2017-07-17 08:59:33 +0200

    Adding Libs

--- src/jdbc/automic/restconnector/IRestAction.java
+++ src/jdbc/automic/restconnector/IRestAction.java
@@ -1,5 +1,15 @@
 package jdbc.automic.restconnector;
 
+import java.sql.ResultSet;
+
+import org.json.simple.JSONArray;
+
 public interface IRestAction {
 	public void action(JSONArray array);
+	
+	public static JSONArray fetchData(ResultSet set) {
+		JSONArray array = null;
+		
+		return array;
+	}
 }
