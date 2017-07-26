10288a9631d4dfde184f755e67ca04febe5b8e52
nic <Christoph.Niederer@automic.com>
2017-07-26 14:15:56 +0200

    unit testing

---
+++ src/test/jdbc/automic/restconnector/RestConnectorTest.java
@@ -0,0 +1,11 @@
+package jdbc.automic.restconnector;
+
+        import org.junit.Assert;
+        import org.junit.Test;
+
+public class RestConnectorTest {
+    @Test
+    public void testtest(){
+        Assert.assertEquals(2, 1+1);
+    }
+}
