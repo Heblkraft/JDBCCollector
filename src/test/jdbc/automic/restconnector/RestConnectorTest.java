f0ad4f1bfd78ed1d744043b3117bb6261c4e9c8f
nic <Christoph.Niederer@automic.com>
2017-07-26 14:41:57 +0200

    adding Mockrunner

--- src/test/jdbc/automic/restconnector/RestConnectorTest.java
+++ src/test/jdbc/automic/restconnector/RestConnectorTest.java
@@ -6,6 +6,11 @@ package jdbc.automic.restconnector;
 public class RestConnectorTest {
     @Test
     public void testtest(){
-        Assert.assertEquals(2, 1+1);
+
+    }
+
+    @Test
+    public void testFetchdata(){
+
     }
 }
