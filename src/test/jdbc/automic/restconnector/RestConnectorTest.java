d33b20323a5b9a24e868766e5aeb0ec240db6fe1
nic <Christoph.Niederer@automic.com>
2017-07-27 09:29:31 +0200

    adding Mockrunner tests

--- src/test/jdbc/automic/restconnector/RestConnectorTest.java
+++ src/test/jdbc/automic/restconnector/RestConnectorTest.java
@@ -1,7 +1,17 @@
 package jdbc.automic.restconnector;
 
+        import com.mockrunner.mock.jdbc.MockResultSet;
+        import javafx.util.converter.TimeStringConverter;
+        import jdbc.automic.configuration.ConfigLoader;
+        import org.json.simple.JSONArray;
+        import org.json.simple.JSONObject;
         import org.junit.Assert;
-        import org.junit.Test;
+        import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.sql.SQLException;
+        import java.sql.Time;
+        import java.sql.Timestamp;
 
 public class RestConnectorTest {
     @Test
@@ -9,8 +19,31 @@ public class RestConnectorTest {
 
     }
 
+    @BeforeClass
+    public static void loadConfig(){
+        ConfigLoader.load("./dbconnection.properties", "./restconnection.properties");
+    }
+
     @Test
     public void testFetchdata(){
+        MockResultSet resultSet = new MockResultSet("hi");
+        resultSet.addColumn("id");
+        resultSet.addColumn("name");
+        resultSet.addColumn("timestamp");
+        resultSet.addRow(new Object[] {1, "Leanne Graham", new Timestamp(1501139662586l)});
 
+        JSONArray array = new JSONArray();
+            JSONObject obj = new JSONObject();
+                obj.put("name", "Leanne Graham");
+                obj.put("id", 1);
+                obj.put("timestamp", new Timestamp(1501139662586l));
+            array.add(obj);
+
+
+        try {
+            Assert.assertEquals(array, IRestAction.fetchData(resultSet));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
